739bd18cc003bf211b551e1bd88a72f666a8e5c8
westerntoad <abr.engebretson@gmail.com>
2024-08-12 19:31:52 -0700

    remove unnecessary code

--- deprecated/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { signup, login } = require('../controllers/authController');
-const router = express.Router();
-
-// handles POST (sending data to the server) requests from /signup and /login endpoints
-router.post('/signup', signup);
-router.post('/login', login);
-
-module.exports = router;
