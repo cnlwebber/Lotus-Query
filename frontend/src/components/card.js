f140de0db75f3eb748073eace4c6ef4e85672d78
westerntoad <abr.engebretson@gmail.com>
2024-08-13 14:43:48 -0700

    Added slideshow and random card component

--- frontend/src/components/card.js
+++ frontend/src/components/card.js
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { randomCard } from '../api.js';
 
+export const RandCardButton = () => {
+    const [randCards, setRandCards] = useState([]);
+
+    useEffect(() => {
+        const getCards = async () => {
+            const response = await randomCard();
+            console.log(response.data)
+            setRandCards(response.data);
+        };
+        getCards();
+    }, []);
+
+    console.log(randCards)
+    return (
+      <div>
+      {randCards.map(card => (
+        <CardButton key={card.uuid} scryfall_id={card.scryfall_id} name={card.name} />
+      ))}
+      </div>
+  );
+}
 
 const CardButton = ({ scryfall_id, name }) => {
     const fileFace = 'front';
@@ -9,7 +31,6 @@ const CardButton = ({ scryfall_id, name }) => {
     const dir1 = scryfall_id.charAt(0);
     const dir2 = scryfall_id.charAt(1);
     const image = `https://cards.scryfall.io/${fileType}/${fileFace}/${dir1}/${dir2}/${scryfall_id}.${fileFormat}`;
-    console.log(image)
     return (
         <Link to={`/card/${scryfall_id}`} className="cardButton">
             <img src={image} alt={name} classname={`cardImage`}></img>
@@ -18,3 +39,27 @@ const CardButton = ({ scryfall_id, name }) => {
 };
 
 export default CardButton;
+
+
+
+/*const [randCards, setRandCards] = useState([]);
+
+  useEffect(() => {
+    const getCards = async () => {
+      const response = await fetchCards();
+      console.log(response.data)
+      setRandCards(response.data);
+    };
+    getCards();
+  }, []);
+
+  return (
+    <div className="homePage">
+      <SearchBar></SearchBar>
+      <div className="randomCards">
+        {randCards.map(card => (
+          <CardButton key={card.uuid} scryfall_id={card.scryfall_id} name={card.name} />
+        ))}
+      </div>
+    </div>
+  );*/
--- frontend/src/pages/Random.js
+++ frontend/src/pages/Random.js
@@ -1,24 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import CardButton from '../components/card.js';
+import RandomCard from '../components/RandomCard.js';
 import { randomCard } from '../api.js';
 
 const Random = () => {
-  const [randCards, setRandCard] = useState([]);
-
-  useEffect(() => {
-    const getCards = async () => {
-      const response = await randomCard();
-      console.log(response.data)
-      setRandCard(response.data);
-    };
-    getCards();
-  }, []);
 
   return (
     <div className="randomCards">
-      {randCards.map(card => (
-        <CardButton key={card.uuid} scryfall_id={card.scryfall_id} name={card.name} />
-      ))}
+      <RandomCard />
     </div>
   );
 };
