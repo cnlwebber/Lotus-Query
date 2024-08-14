1b31884a75d48d9234c60767a94808d748e729b0
cnlwebber <cnlwebber@gmail.com>
2024-08-14 04:25:16 -0700

    C:/Program Files/Git/random redirects to /scry/{RANDOMCARD.scryfall_id}

--- frontend/src/pages/Random.js
+++ frontend/src/pages/Random.js
@@ -1,14 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import RandomCard from '../components/RandomCard.js';
+import { useEffect } from 'react';
 import { randomCard } from '../api.js';
+import { useNavigate } from 'react-router-dom';
 
 const Random = () => {
+  const navigate = useNavigate();
 
-  return (
-    <div className="randomCards">
-      <RandomCard />
-    </div>
-  );
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const response = await randomCard();
+        const id = response.data[0].scryfall_id;
+        const path = `/scry/${id}`
+        navigate(path)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    fetch();
+  }, [navigate])
+
+  return null
+
 };
 
 export default Random;
